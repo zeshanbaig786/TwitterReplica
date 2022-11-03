44981f453716d36b493bea21e587e6206ec56df6
zeshanbaig786 <zeshanbaig786@gmail.com>
2022-11-03 16:16:12 +0500

    user register, login done

--- TwitterReplica/TwitterReplica/views.py
+++ TwitterReplica/TwitterReplica/views.py
@@ -3,35 +3,143 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+
 from TwitterReplica import app
 
-@app.route('/')
-@app.route('/home')
-def home():
-    """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-    )
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
 
-@app.route('/contact')
+import re
+
+app.secret_key = 'SessionSecretKey'
+
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "P@kistan@123"
+app.config["MYSQL_DB"] = "acp_assignment1"
+
+mysql = MySQL(app)
+
+
+@app.route("/")
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    msg = ""
+    if (
+        request.method == "POST"
+        and "username" in request.form
+        and "password" in request.form
+    ):
+        username = request.form["username"]
+        password = request.form["password"]
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute(
+            "SELECT * FROM user WHERE username = % s AND password = % s",
+            (
+                username,
+                password,
+            ),
+        )
+        account = cursor.fetchone()
+        if account:
+            session["loggedin"] = True
+            session["id"] = account["id"]
+            session["username"] = account["username"]
+            msg = "Logged in successfully !"
+            return render_template("index.html", msg=msg)
+        else:
+            msg = "Incorrect username / password !"
+    return render_template("login.html", msg=msg)
+
+
+@app.route("/logout")
+def logout():
+    session.pop("loggedin", None)
+    session.pop("id", None)
+    session.pop("username", None)
+    return redirect(url_for("login"))
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    msg = ""
+    if (
+        request.method == "POST"
+        and "username" in request.form
+        and "password" in request.form
+        and "email" in request.form
+    ):
+        username = request.form["username"]
+        password = request.form["password"]
+        email = request.form["email"]
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("SELECT * FROM user WHERE username = % s", (username,))
+        account = cursor.fetchone()
+        if account:
+            msg = "Account already exists !"
+        elif not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            msg = "Invalid email address !"
+        elif not re.match(r"[A-Za-z0-9]+", username):
+            msg = "Username must contain only characters and numbers !"
+        elif not username or not password or not email:
+            msg = "Please fill out the form !"
+        else:
+            q = (
+                "INSERT INTO user(username,email,password) VALUES(%s,%s,%s);",
+                (username, email, password),
+            )
+            # cursor.execute('INSERT INTO user VALUES (NULL, % s, % s, % s)', (username, password, email, ))
+            cursor.execute(
+                "INSERT INTO user(username,email,password) VALUES(%s,%s,%s);",
+                (username, email, password),
+            )
+            mysql.connection.commit()
+            msg = "You have successfully registered !"
+    elif request.method == "POST":
+        msg = "Please fill out the form !"
+    return render_template("register.html", msg=msg)
+
+
+@app.route("/home")
+def home():
+    try:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("SELECT * FROM posts where createdBy=%s",session["id"])
+        rows = cursor.fetchall()
+        table = Results(rows)
+
+        """Renders the home page."""
+        return render_template(
+            "index.html",
+            title="Home Page",
+            year=datetime.now().year,
+            table=table
+        )
+    except e:
+        print(e)
+    finally:
+        cursor.close()
+
+
+
+@app.route("/contact")
 def contact():
     """Renders the contact page."""
     return render_template(
-        'contact.html',
-        title='Contact',
+        "contact.html",
+        title="Contact",
         year=datetime.now().year,
-        message='Your contact page.'
+        message="Your contact page.",
     )
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
     """Renders the about page."""
     return render_template(
-        'about.html',
-        title='About',
+        "about.html",
+        title="About",
         year=datetime.now().year,
-        message='Your application description page.'
+        message="Your application description page.",
     )
